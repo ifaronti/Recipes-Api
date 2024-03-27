8429aa1bb6ae0179ed17449d5fcfadb2bf79888c
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-03-27 21:20:37 +0000

    fine tuning

--- recipes/src/components/recipesApp/nav.js
+++ recipes/src/components/recipesApp/nav.js
@@ -1,10 +1,12 @@
 import SearchInput from "./searchInput";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Nav(){
+    const goto = useNavigate()
  return(
         <nav>
-            <h1 className="logo"><i>Ifarontimi</i></h1>
+          <h1 onClick={()=> goto('/')} className="logo"><i>Ifarontimi</i></h1>
             <ul>
                 <li><Link className='links' to='/'>Home</Link></li>
                 
